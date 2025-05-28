b6852a068cf2ca1d45b7f53755eeac7a24ccb8ef
AbinChristopher <abchristythoppil@gmail.com>
2025-05-28 12:02:33 +1200

    Front End

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import store from './redux/store';
-import NavMenu from './components/navmenu/NavMenu';
-import CustomerPage from './pages/CustomerPage';
-import StorePage from './pages/StorePage';
+import NavMenu from './components/navmenu/NavMenu.jsx';
+import CustomerPage from './pages/CustomerPage.jsx';
+import StorePage from './pages/StorePage.jsx';
 import SalesPage from './pages/SalesPage';
 import ProductsPage from './pages/ProductsPage';
 import HomePage from './pages/HomePage';
